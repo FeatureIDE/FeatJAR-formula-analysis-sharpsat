443d484a026b8e8a6ab4efbf67d9ef8781bc4b6b
Sebastian Krieter <sebastian.krieter@ovgu.de>
2021-09-24 14:07:36 +0200

    Added uniform random sampling using sharpSat.

--- src/main/java/org/spldev/formula/configuration/sample/DistributionMetrics.java
+++ src/main/java/org/spldev/formula/configuration/sample/DistributionMetrics.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Formula-Analysis Lib - Library to analyze propositional formulas.
+ * Formula-Analysis-SharpSat Lib - Library to analyze propositional formulas with SharpSAT.
  * Copyright (C) 2021  Sebastian Krieter
  * 
- * This file is part of Formula-Analysis Lib.
+ * This file is part of Formula-Analysis-SharpSat Lib.
  * 
- * Formula-Analysis Lib is free software: you can redistribute it and/or modify it
+ * Formula-Analysis-SharpSat Lib is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  * 
- * Formula-Analysis Lib is distributed in the hope that it will be useful,
+ * Formula-Analysis-SharpSat Lib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Formula-Analysis Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Formula-Analysis-SharpSat Lib.  If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/formula-analysis> for further information.
+ * See <https://github.com/skrieter/formula-analysis-sharpsat> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.formula.configuration.sample;
