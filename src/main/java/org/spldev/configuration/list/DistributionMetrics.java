bb2f4949bc3e8054899cd5847f72f5f0301dc610
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:00 +0200

    Updated license headers

--- src/main/java/org/spldev/configuration/list/DistributionMetrics.java
+++ src/main/java/org/spldev/configuration/list/DistributionMetrics.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Formula-Analysis-SharpSat Lib - Library to analyze propositional formulas with SharpSAT.
- * Copyright (C) 2021  Sebastian Krieter
+ * formula-analysis-sharpsat - Analysis of propositional formulas using sharpSAT
+ * Copyright (C) 2021 Sebastian Krieter
  * 
- * This file is part of Formula-Analysis-SharpSat Lib.
+ * This file is part of formula-analysis-sharpsat.
  * 
- * Formula-Analysis-SharpSat Lib is free software: you can redistribute it and/or modify it
+ * formula-analysis-sharpsat is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Formula-Analysis-SharpSat Lib is distributed in the hope that it will be useful,
+ * formula-analysis-sharpsat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Formula-Analysis-SharpSat Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with formula-analysis-sharpsat. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/formula-analysis-sharpsat> for further information.
+ * See <https://github.com/FeatJAR/formula-analysis-sharpsat> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.configuration.list;
