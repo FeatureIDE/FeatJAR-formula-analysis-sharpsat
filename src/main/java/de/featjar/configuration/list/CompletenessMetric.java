3bc214d5a226e99d00e9a87dd72ae8713500d21a
Elias Kuiter <info@elias-kuiter.de>
2022-08-13 11:03:37 +0200

    License

--- src/main/java/de/featjar/configuration/list/CompletenessMetric.java
+++ src/main/java/de/featjar/configuration/list/CompletenessMetric.java
@@ -1,24 +1,22 @@
-/* -----------------------------------------------------------------------------
- * formula-analysis-sharpsat - Analysis of propositional formulas using sharpSAT
- * Copyright (C) 2021 Sebastian Krieter
- * 
+/*
+ * Copyright (C) 2022 Sebastian Krieter
+ *
  * This file is part of formula-analysis-sharpsat.
- * 
+ *
  * formula-analysis-sharpsat is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
- * 
+ *
  * formula-analysis-sharpsat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with formula-analysis-sharpsat. If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  * See <https://github.com/FeatJAR/formula-analysis-sharpsat> for further information.
- * -----------------------------------------------------------------------------
  */
 package de.featjar.configuration.list;
 
